28867f6607db9642a4b64b9da17923b834d6ea0c
Alexis Vandewalle <a.vandewalle@isae.fr>
2020-04-27 22:29:38 +0200

    split fonctionnel

--- model.py
+++ model.py
@@ -1,14 +1,7 @@
 #importations
-import networkx as nx
-import matplotlib.pyplot as plt
-import random as rd
-import numpy as np
 from time import time
-from user import *
-from tangle import *
 from simulation import *
-from transaction import *
-from settings import *
+
 
 #simulation
 simulation = SimulationSplit()
